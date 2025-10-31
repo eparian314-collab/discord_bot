3aa0e4a3736a4234f3dd9d4c187be1525cf95e0f
eparian314 <e.parian314@gmail.com>
2025-10-30 18:58:21 -0700

    ðŸ§  Auto update (2025-10-30 18:58:21): Sync latest engine/data/test changes

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -8,7 +8,7 @@ from .role_utils import (
     is_server_owner,
 )
 from .channel_utils import (
-    get_bot_channel_id,
+    get_bot_channel_ids,  # Updated from get_bot_channel_id
     get_sos_channel_id,
     find_bot_channel,
     find_sos_channel,
@@ -22,7 +22,7 @@ __all__ = [
     "has_helper_role",
     "is_admin_or_helper",
     "get_helper_role_id",
-    "get_bot_channel_id",
+    "get_bot_channel_ids",  # Updated from get_bot_channel_id
     "get_sos_channel_id",
     "find_bot_channel",
     "find_sos_channel",
